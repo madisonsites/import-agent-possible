dc0590e2d654bca16049b5e8f0e410de7de54324
Madison Sites <madison.sites@gmail.com>
2016-07-01 11:41:37 -0700

    Add tests to clarify agent associations

--- test/models/agent_test.rb
+++ test/models/agent_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AgentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be able to call office_mls" do
+    assert "142246", agents(:one).office_mls
+  end
+  test "should be able to call its own mls" do
+    assert "881383", agents(:one).mls
+  end
 end
