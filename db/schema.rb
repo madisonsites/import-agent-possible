3133cd43614d8418856db83f502ec220c37c46c8
Madison Sites <madison.sites@gmail.com>
2016-07-01 13:18:17 -0700

    Adds updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701180525) do
+ActiveRecord::Schema.define(version: 20160701183854) do
 
   create_table "agents", force: :cascade do |t|
     t.string   "first_name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20160701180525) do
     t.string   "email"
     t.string   "description"
     t.string   "image_url"
-    t.string   "agent_mls"
+    t.string   "mls"
     t.string   "phone_number"
     t.string   "country_code"
     t.datetime "created_at",   null: false
