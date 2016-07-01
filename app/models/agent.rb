b961b05475273b63be1a3fb8d04f94e32f844935
Madison Sites <madison.sites@gmail.com>
2016-07-01 12:38:45 -0700

    Adds unique mls and email validations on agents

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,4 +1,7 @@
 class Agent < ApplicationRecord
+  validates :mls, presence: true, uniqueness: true
+  # TIL about custom validators; using this instead of format: to allow for adding in bad domains like mailinator
+  validates :email, presence: true, email: true
   belongs_to :office
   delegate :mls, to: :office, prefix: true, allow_nil: true
 end
