bf17273c5afcf0e7b2cbb28a71ef3eab7f69e887
Madison Sites <madison.sites@gmail.com>
2016-07-01 13:15:20 -0700

    Add invalid email teset

--- test/models/agent_test.rb
+++ test/models/agent_test.rb
@@ -7,4 +7,11 @@ class AgentTest < ActiveSupport::TestCase
   test "should be able to call its own mls" do
     assert "881383", agents(:one).mls
   end
+  test "should not allow invalid email" do
+    bad_emails = ["madison", "madison@", "madison.com"]
+    bad_emails.each do |bad_email|
+      agent = Agent.new(email: bad_email, mls: "123456")
+      assert_equal true, agent.invalid?(:email)
+    end
+  end
 end
