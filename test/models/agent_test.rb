50b91ae8ef873e79d805bb47910d36083892597e
Madison Sites <madison.sites@gmail.com>
2016-07-04 16:15:06 -0700

    Add test for stripping html

--- test/models/agent_test.rb
+++ test/models/agent_test.rb
@@ -15,4 +15,10 @@ class AgentTest < ActiveSupport::TestCase
       assert_equal true, agent.invalid?
     end
   end
+  test "should strip html tags" do
+    agents(:one).description = "<p>Lou is a CPA in the City of Chicago&mdash;so getting his own broker&rsquo;s license and building a realty practice has been a natural progression.&nbsp;Lou thoroughly understands his clients&rsquo; goals.</p><p>He received an MBA in Finance &amp; Business Policy from the University of Chicago. Lou speaks three languages, owing to his Cuban heritage and his wife&rsquo;s Brazilian roots. Lou has kids ranging from 11 months to 24 years &mdash;while the oldest is a third year medical student, the youngest is just learning to walk!Lou finds that the most rewarding part of being a Dream Town real estate agent is helping someone find their new&mdash;possibly first&mdash;home where they, too, can escape to their personal oasis.</p>"
+    agents(:one).save
+    assert_equal "Lou is a CPA in the City of Chicago—so getting his own broker’s license and building a realty practice has been a natural progression. Lou thoroughly understands his clients’ goals.He received an MBA in Finance & Business Policy from the University of Chicago. Lou speaks three languages, owing to his Cuban heritage and his wife’s Brazilian roots. Lou has kids ranging from 11 months to 24 years —while the oldest is a third year medical student, the youngest is just learning to walk!Lou finds that the most rewarding part of being a Dream Town real estate agent is helping someone find their new—possibly first—home where they, too, can escape to their personal oasis.",
+                agents(:one).description
+  end
 end
